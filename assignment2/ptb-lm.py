843d2da37479b23c7b38429bf0fed9002164cca3
Jonathan Plante <plante.j.g@gmail.com>
2019-03-18 19:54:30 -0400

    for the graphs
    updated times

--- assignment2/ptb-lm.py
+++ assignment2/ptb-lm.py
@@ -52,7 +52,7 @@
 #          TRANSFORMER:  train:  67  val: 146
 #    - For Problem 4.2 (exploration of optimizers), you will make use of the 
 #      experiments from 4.1, and should additionally run the following experiments:
-#          --model=RNN --optimizer=SGD --initial_lr=0.0001 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 
+#          --model=RNN --optimizer=SGD --initial_lr=0.0001 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 train :    val : 
 #          --model=GRU --optimizer=SGD --initial_lr=10 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35
 #          --model=TRANSFORMER --optimizer=SGD --initial_lr=20 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=.9
 #          --model=RNN --optimizer=SGD_LR_SCHEDULE --initial_lr=1 --batch_size=20 --seq_len=35 --hidden_size=512 --num_layers=2 --dp_keep_prob=0.35
@@ -197,7 +197,7 @@ else:
 ###############################################################################
 #
 # 
-LOADING & PROCESSING
+# DATA LOADING & PROCESSING
 #
 ###############################################################################
 
@@ -492,7 +492,8 @@ print('\nDONE\n\nSaving learning curves to '+lc_path)
 np.save(lc_path, {'train_ppls':train_ppls, 
                   'val_ppls':val_ppls, 
                   'train_losses':train_losses,
-                  'val_losses':val_losses})
+                  'val_losses':val_losses,
+				  'times':times})
 # NOTE ==============================================
 # To load these, run 
 # >>> x = np.load(lc_path)[()]
